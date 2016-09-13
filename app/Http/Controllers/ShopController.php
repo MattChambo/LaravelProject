9b09ae60e80bb053f37baed43ee113d87d19140b
Matthew Chamberlain <matthewwillliamchamberlain@gmail.com>
2016-09-14 11:26:57 +1200

    Update

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Products;
 
 class ShopController extends Controller
 {
@@ -20,6 +21,18 @@ class ShopController extends Controller
     	$this->validate($request,[
     		'product_title'=>'required|min:1|max:255',
     		'product-description'=>'required|min:10',
+            //'product_image'=>'required|image',
+            'product_price'=>'required|numeric',
+            'product_quantity'=>'required|numeric'
     		]);
+
+        $newProduct = new Products();
+        $newProduct->title = $request->product_title;
+        $newProduct->description = $request->description;
+        $newProduct->price = $request->product_price;
+        $newProduct->quantity = $request->product_quantity;
+
+        // $newProduct->save();
+        return redirect('/Shop');
     }
 }
--- resources/views/shop/add.blade.php
+++ resources/views/shop/add.blade.php
@@ -19,29 +19,37 @@
 	{!! csrf_field() !!}
 
 
-	<div class="form-group">
+	<div class="form-group {{ $errors->has('product_title')? 'has-error' :'' }}">
 		<label>Product Title</label>
 		<input type="text" class="form-control" name="product_title" value="{{ old('product_title') }}">
+		{!!$errors->first('product_title','<span class="help-block">:message</span>')  !!}
 	</div>
 
-	<div class="form-group">
+	<div class="form-group {{ $errors->has('product-description')? 'has-error' :'' }}">
 		<label>Product Description</label>
 		<textarea name="product-description" class="form-control" placeholder="Product description" row="5">{{ old('product-description') }}</textarea>
+		{!!$errors->first('product-description','<span class="help-block">:message</span>')  !!}
+	</div>
+	<div class="form-group {{ $errors->has('product_image')? 'has-error' :'' }}">
+		<label>Product image</label>
+		<input type="file" class="form-control" name="product_image" value="{{ old('product_image') }}">
+		{!!$errors->first('product_image','<span class="help-block">:message</span>')  !!} 
+	</div>
+	<div class="form-group {{ $errors->has('product_price')? 'has-error' :'' }}">
+		<label>Product Price</label>
+		<input type="number" class="form-control" name="product_price" value="{{ old('product_price') }}">
+		{!!$errors->first('product_price','<span class="help-block">:message</span>')  !!} 
+	</div>
+	<div class="form-group {{ $errors->has('product_quantity')? 'has-error' :'' }}">
+		<label>Product Quantity</label>
+		<input type="number" class="form-control" name="product_quantity" value="{{ old('product_quantity') }}">
+		{!!$errors->first('product_quantity','<span class="help-block">:message</span>')  !!} 
 	</div>
 
 	<div class="form-group">
 		<button type="submit" class="btn btn-primary">Add Product</button>
 	</div>
 </form>
-
-@if(count($errors))
-	<ul>
-		@foreach($errors->all() as $error)
-		<li>{{ $error }}</li>
-		@endforeach
-	</ul>
-
-@endif
 
 @endsection
 
